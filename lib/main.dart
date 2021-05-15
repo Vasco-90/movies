04482dcccd0a33862311067e0ddce6e3d2424a33
Christian Dobrovolny <christian.dobrovolny@gmx.de>
2021-05-15 18:08:59 +0200

    add some example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(TheMovieApp());
+}
+
+class TheMovieApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('The Movie App'),
+        ),
+      ),
+    );
+  }
 }
